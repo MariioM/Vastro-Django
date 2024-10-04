55cf6d24995c806390bf0f44af5e9dcb09a90b12
Mario Martínez Vitutia <9205625@alumnos.ufv.es>
2024-10-04 11:23:10 +0200

    Settings modificado borrado implicito Autobuild

--- project/settings.py
+++ project/settings.py
@@ -133,6 +133,3 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-#Deploy
-SCM_DO_BUILD_DURING_DEPLOYMENT=True
